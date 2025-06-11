618c33177fce22586efa9ff61d360cadbc6baf15
Erik Stern <erik.stern@outlook.de>
2025-06-12 00:27:05 +0200

    Build agent and core instance show up in eureka instances. Core instance does not register build agent as such though, yet.

--- setup.sh
+++ setup.sh
@@ -4,17 +4,14 @@
 COLOR_INFO='\033[1;34m'   # bright blue
 RESET='\033[0m'
 
-# RELEASE_VERSION=8.1.3
 
-# Download an Artemis release:
-# if ! [ -f artemis.war ]; then
-#     echo -e "${COLOR_INFO}⏳ Downloading Artemis release $RELEASE_VERSION...${RESET}"
-#     curl -L https://github.com/ls1intum/Artemis/releases/download/$RELEASE_VERSION/Artemis.war \
-#          --output artemis.war --no-clobber
-# fi
+# Export docker group id, so the build agent can use the socket.
+DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3)
+echo -e "${COLOR_INFO}Docker Group Id: $DOCKER_GROUP_ID"
+echo "DOCKER_GROUP_ID=$DOCKER_GROUP_ID" > .env
+
 
 # Startup the dependencies using docker-compose:
-echo ""
 echo -e "${COLOR_INFO}⏳ Starting dependencies and all instances using docker-compose...${RESET}"
 
 # Postgres, JHipster and ActiveMQ:
@@ -22,12 +19,3 @@ sudo docker-compose down -v
 sudo docker-compose rm
 sudo rm -rf postgresql-data
 sudo docker-compose up # --detach
-
-# # Start the main Artemis instance.
-# echo ""
-# echo -e "${COLOR_INFO}⏳ Starting artemis main instance...${RESET}"
-# java -jar artemis.war \
-#     --server.port=8080 \
-#     --eureka.instance.instanceId=Artemis:1 \
-#     --spring.profiles.active=prod,core,localvc,localci,scheduling,local \
-#     --spring.config.additional-location=file:files/main-instance/application-local.yml
