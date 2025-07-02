b1e90ad277607f9ba3428a01bae25addf689c620
Erik Stern <erik.stern@outlook.de>
2025-07-02 21:54:10 +0200

    Work.

--- sol-docker-swarm/setup-core-instance.sh
+++ sol-docker-swarm/setup-core-instance.sh
@@ -2,6 +2,6 @@ DOCKER_GROUP_ID=$(getent group docker | cut -d: -f3)
 echo -e "Docker Group Id: $DOCKER_GROUP_ID"
 echo "DOCKER_GROUP_ID=$DOCKER_GROUP_ID" > .env
 
-# docker-compose down -v
-# docker-compose rm
-# docker-compose up artemis-core-instance
+docker-compose down -v
+docker-compose rm
+docker-compose up artemis-core-instance
